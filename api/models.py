cba168a6029728fd05166c39192217cc5c0ce868
Khoross <31031340+Khoross@users.noreply.github.com>
2017-10-16 14:01:17 +0100

    Update models.py
    Change of basic model

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class LACount(models.Model):
+class WordData(models.Model):
     aootype = models.CharField(max_length=1)
     aookey = models.IntegerField()
     aooname = models.CharField(max_length=255)
@@ -11,31 +11,5 @@ class LACount(models.Model):
     income = models.FloatField(blank=True, null=True)
     charity_count = models.IntegerField()
     subsidiary_count = models.IntegerField()
-
-
-class AreaCount(models.Model):
-    aootype = models.CharField(max_length=1)
-    expend = models.FloatField(blank=True, null=True)
-    income = models.FloatField(blank=True, null=True)
-    charity_count = models.IntegerField()
-    subsidiary_count = models.IntegerField()
-
-
-class LAWords(models.Model):
-    aootype = models.CharField(max_length=1)
-    aookey = models.IntegerField()
-    aooname = models.CharField(max_length=255)
-    word = models.CharField(max_length=255)
-    expend = models.FloatField(blank=True, null=True)
-    income = models.FloatField(blank=True, null=True)
-    charity_count = models.IntegerField()
-    subsidiary_count = models.IntegerField()
-
-
-class AreaWords(models.Model):
-    aooname = models.CharField(max_length=255)
-    word = models.CharField(max_length=255)
-    expend = models.FloatField(blank=True, null=True)
-    income = models.FloatField(blank=True, null=True)
-    charity_count = models.IntegerField()
-    subsidiary_count = models.IntegerField()
+    _calc_type = models.SmallIntegerField(db_column='calc_type')
+    calc_types=["tf.idf", "Relative weighted frequency", "Weighted frequency", "Total"]
