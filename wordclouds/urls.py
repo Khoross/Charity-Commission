5890aa4ef76c2ab4cec1fbe46118ae5e9f22da1f
Khoross <31031340+Khoross@users.noreply.github.com>
2017-10-16 14:40:09 +0100

    Update urls.py

--- wordclouds/urls.py
+++ wordclouds/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from tastypie.api import Api
+from api.resources impot WordDataResource
+
+v1_api = Api(api_name='v1')
+v1_api.register(WordDataResource())
 
 urlpatterns = [
     url(r'^clouds/', include('clouds.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'api/', include(v1_api.urls)),
 ]
