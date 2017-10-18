c0a8a8e3748a23afd3a12a1a7373a487ac4b367b
Khoross <31031340+Khoross@users.noreply.github.com>
2017-10-18 14:19:02 +0100

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+def toggle_session(request):
+    if not request.is_ajax() or not request.method=='POST':
+        return HttpResponseNotAllowed(['POST'])
+    try:
+        data = request.POST
+        if request.session[data['aootype']] is None:
+            request.session[data['aootype']] = {data['word']: True}
+        else:
+            request.session[data['aootype']]\
+                   .update({data['word']: not request.session[data['aootype']]
+                                                     .get(data['word'], False)}
+            request.session.modified = True
+    return HttpResponse('ok')
