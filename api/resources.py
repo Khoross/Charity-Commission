c7406b7cef15b8b1fbb999403fcd470f733d5b6b
Khoross <31031340+Khoross@users.noreply.github.com>
2017-10-18 12:15:09 +0100

    Update resources.py

--- api/resources.py
+++ api/resources.py
@@ -4,14 +4,33 @@ from .models import *
 
 
 class WordDataResource(ModelResource):
+    hidden = fields.BooleanField(readonly=True)
     class Meta:
         queryset = WordData.objects.all()
         limit = 0
         filtering = {'aootype': ALL,
                      'aookey': ALL,
                      'aooname': ALL,
-                     'calc_type': ALL, }
+                     'calc_type': ALL,
+                     'word': ALL}
         ordering = ['expend', 'income', 'charity_count', 'subsidiary_count']
 
     def dehydrate_calc_type(self, bundle):
         return(WordData.calc_type_strings[bundle.data['calc_type']])
+    
+    def dehydrate_hidden(self, bundle):
+        return(bundle.request
+                     .session
+                     .get(bundle.data['aootype'], {})
+                     .get(bundle.data['word'], False))
+    
+    def dehydrate(self, bundle):
+        if hasattr(bundle.request, 'GET'):
+            if bundle.request.GET.get('weight', None) is not None and 
+               bundle.request.GET.get('weight', '') in bundle.data.keys():
+                bundle.data['weight'] = bundle.data[bundle.request.GET.get('weight')]
+                del bundle.data['income']
+                del bundle.data['expend']
+                del bundle.data['charity_count']
+                del bundle.data['subsidiary_count']
+        return(bundle)
